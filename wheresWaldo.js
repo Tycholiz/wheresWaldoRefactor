641a08255bf9817c1500dcda5ceb809ede21fbc7
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-08-02 18:51:08 +0000

    changed the Waldo program to use forEach loop

--- wheresWaldo.js
+++ wheresWaldo.js
@@ -1,14 +1,13 @@
-// The second argument/parameter is expected to be a function
+
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(name) {
+    if (name === "Waldo") {
+      found(arr.indexOf(name));   // execute callback
     }
-  }
+  });
 }
 
 function actionWhenFound(index) {
-
   console.log("Found Waldo at index " + index + "!");
 }
 
